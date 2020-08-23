f918363a87ed8c69a3290c2ee0c7d0091a2e82ad
Ivan6274 <Isivanov2011@gmail.com>
2020-08-23 15:09:35 +0300

    MileApp added

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketcoast = (int) 1233.77F;
+        int mile = 20;
+        long totalmile = ticketcoast/mile;
+        System.out.println(totalmile);
+
+    }
+}
+
+
